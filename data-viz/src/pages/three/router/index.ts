557824861393224977cf945bb49e78ca8eda7938
VfanLee <fanfanfafafa@gmail.com>
2025-03-15 17:14:41 +0800

    feat: three.js

---
+++ data-viz/src/pages/three/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/quick-start',
+      name: 'quick-start',
+      component: () => import('../views/QuickStart.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ data-viz/src/pages/three/views/QuickStart.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import * as THREE from 'three'
+
+function init() {
+  // 创建场景
+  const scene = new THREE.Scene()
+  // 创建相机
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.z = 3
+  camera.position.x = 2
+  camera.lookAt(0, 0, 0)
+  // 创建渲染器
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial())
+  scene.add(cube)
+
+  function animate() {
+    requestAnimationFrame(animate)
+
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
+  animate()
+}
+
+onMounted(() => {
+  init()
+})
+</script>
+
+<template>
+  <div class="box"></div>
+</template>
+
+<style lang="scss" scoped></style>
