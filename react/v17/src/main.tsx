7326cc699c7bfc6ad51fca0c104d536c96cf1734
VfanLee <fanfanfafafa@gmail.com>
2025-04-12 17:44:41 +0800

    refactor(react): version classification

---
+++ react/v17/src/main.tsx
@@ -0,0 +1,13 @@
+import 'antd/dist/antd.css'
+
+import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+
+import App from './App.tsx'
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+)
